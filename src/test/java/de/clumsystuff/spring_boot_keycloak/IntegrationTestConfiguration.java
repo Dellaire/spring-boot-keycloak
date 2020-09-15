65108fbdd45fd066b136c21b7903330407268a87
henb <henb@spreadshirt.net>
2020-09-15 07:28:37 +0200

    fixed Javadoc

--- src/test/java/de/clumsystuff/spring_boot_keycloak/IntegrationTestConfiguration.java
+++ src/test/java/de/clumsystuff/spring_boot_keycloak/IntegrationTestConfiguration.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Bean;
 public class IntegrationTestConfiguration {
 
     /**
-     * This bean is used by Keycloak, it holds the properties required to operate with it. When running the
-     * application in a normal way, there is an instance available. However, for some reason, there is
+     * This bean is used by the Keycloak adapter, it holds the properties required to operate with Keycloak. When
+     * running the application in a normal way, there is an instance available. However, for some reason, there is
      * no instance available for integration tests, so the test would run into a NPE. This mocked instance is a
      * workaround.
      *
